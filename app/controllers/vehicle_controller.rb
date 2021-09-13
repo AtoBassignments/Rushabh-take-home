87cf96ca77fcfe1859302a2a50a9369e61c5a4e4
rushabh <rushabh@browserstack.com>
2021-09-14 01:48:59 +0530

    FIX: controller name

--- app/controllers/vehicle_controller.rb
+++ app/controllers/vehicle_controller.rb
@@ -1,4 +1,4 @@
-class HomepageController < ApplicationController
+class VehicleController < ApplicationController
   def latest_fleet_location
     fleet_data = Samsara::ApiClient.fetch_fleet_status
     updated_vehicles = Vehicle.update_vehicles(fleet_data)
