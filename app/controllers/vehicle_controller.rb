9c9a4623816393c1acd59301581b8d5184245e5b
rushabh <rushabh@browserstack.com>
2021-09-14 01:51:44 +0530

    removing unnecessary controller

--- app/controllers/vehicle_controller.rb
+++
@@ -1,7 +0,0 @@
-class VehicleController < ApplicationController
-  def latest_fleet_location
-    fleet_data = Samsara::ApiClient.fetch_fleet_status
-    updated_vehicles = Vehicle.update_vehicles(fleet_data)
-    ActionCable.server.broadcast("fleet_status", {updated_vehicles: updated_vehicles})
-  end
-end
